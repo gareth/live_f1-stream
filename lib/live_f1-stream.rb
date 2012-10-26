af9d609ee5d773bee3b24c38d4b8184cb4244b15
Gareth Adams <gareth.adams@gmail.com>
2012-10-26 15:19:46 +0100

    Skeleton gem

---
+++ lib/live_f1-stream.rb
@@ -0,0 +1,7 @@
+require "live_f1-stream/version"
+
+module LiveF1
+  module Stream
+    # Your code goes here...
+  end
+end
---
+++ lib/live_f1-stream/version.rb
@@ -0,0 +1,5 @@
+module LiveF1
+  module Stream
+    VERSION = "0.0.1"
+  end
+end
